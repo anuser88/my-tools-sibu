eb11f62327c79b22aaf4c26278594ddef67dbca9
ezx6t <166784640+anuser88@users.noreply.github.com>
2026-09-22 14:54:55 +0700

    Clean up request headers and body in bdw.js
    Removed unnecessary headers and cleared username and sessionToken in the request body.

---
+++ bdw.js
@@ -0,0 +1,14 @@
+fetch("https://app.studiodanv.workers.dev/api/project/action", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+    "content-type": "application/json",
+    "x-client-type": "StudioDANV-Web"
+  },
+  "referrer": "https://turbows.pages.dev/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"fileId\":\"BQACAgUAAyEGAAMBAm7g1wACB-Rqsf05RcndeGUwR-TiVKU8f5eO2wAC4h4AAroAAZBVQNOnos2qs789BA\",\"action\":\"add_view\",\"username\":\"\",\"sessionToken\":\"\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
