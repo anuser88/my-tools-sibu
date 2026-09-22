6b4a55626d877b956918c06a3a969b6de8cbc9d0
ezx6t <166784640+anuser88@users.noreply.github.com>
2026-09-22 15:25:13 +0700

    Add async function to fetch and process targets

--- bdw.js
+++ bdw.js
@@ -34,3 +34,4 @@
     });
   }
 }();
+// javascript:!async function(){const e=(await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(e=>e.text())).split(/\r?\n/).filter(Boolean);for(;;)for(const o of e)n(o),await t(400);function t(e){return new Promise(t=>setTimeout(t,e))}function n(e){const t=localStorage.getItem("danv_user");t?.username&&fetch("https://app.studiodanv.workers.dev/api/project/action",{headers:{accept:"*/*","accept-language":"vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5","content-type":"application/json","x-client-type":"StudioDANV-Web"},referrer:"https://turbows.pages.dev/",referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify({fileId:e,action:"add_view",username:t.username,sessionToken:t.sessionToken}),method:"POST",mode:"cors",credentials:"omit"})}}();
