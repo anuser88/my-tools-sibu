c4340c9d50dfc5965351c52186a0d559acb081dc
ezx6t <166784640+anuser88@users.noreply.github.com>
2026-09-22 16:40:35 +0700

    Refactor bv function to use targets array

--- bdw.js
+++ bdw.js
@@ -1,16 +1,14 @@
 !async function() {
   const txt = await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(x => x.text());
   const targets = txt.split(/\r?\n/).filter(Boolean);
+  let i = 0;
   while (!0) {
-    for (const target of targets) {
-      bv(target);
-      await sleep(400);
-    }
+    await bv();
   }
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  function bv(fid) {
+  async function bv() {
     const user = JSON.parse(localStorage.getItem("danv_user"));
     if (!user?.username) return;
     fetch("https://app.studiodanv.workers.dev/api/project/action", {
@@ -18,7 +16,7 @@
         "x-client-type": "StudioDANV-Web"
       },
       "body": JSON.stringify({
-        fileId: fid,
+        fileId: targets[i],
         action: "add_view",
         username: user.username,
         sessionToken: user.sessionToken
@@ -26,6 +24,9 @@
       "method": "POST",
       "credentials": "omit"
     });
+    i++;
+    i%=targets.length;
+    await sleep(400);
   }
 }();
-// javascript:!async function(){const t=(await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(t=>t.text())).split(/\r?\n/).filter(Boolean);for(;;)for(const o of t)n(o),await e(400);function e(t){return new Promise(e=>setTimeout(e,t))}function n(t){const e=JSON.parse(localStorage.getItem("danv_user"));e?.username&&fetch("https://app.studiodanv.workers.dev/api/project/action",{headers:{"x-client-type":"StudioDANV-Web"},body:JSON.stringify({fileId:t,action:"add_view",username:e.username,sessionToken:e.sessionToken}),method:"POST",credentials:"omit"})}}();
+// javascript:!async function(){const e=(await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(e=>e.text())).split(/\r?\n/).filter(Boolean);let t=0;for(;;)await s();async function s(){const s=JSON.parse(localStorage.getItem("danv_user"));var n;s?.username&&(fetch("https://app.studiodanv.workers.dev/api/project/action",{headers:{"x-client-type":"StudioDANV-Web"},body:JSON.stringify({fileId:e[t],action:"add_view",username:s.username,sessionToken:s.sessionToken}),method:"POST",credentials:"omit"}),t++,t%=e.length,await(n=400,new Promise(e=>setTimeout(e,n))))}}();
