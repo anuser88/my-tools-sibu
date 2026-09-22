fc13330ea7d7eaed93b88ac79ec2317dd7dd6715
ezx6t <166784640+anuser88@users.noreply.github.com>
2026-09-22 15:39:40 +0700

    Parse user data from localStorage in bv function

--- bdw.js
+++ bdw.js
@@ -11,17 +11,12 @@
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   function bv(fid) {
-    const user = localStorage.getItem("danv_user");
+    const user = JSON.parse(localStorage.getItem("danv_user"));
     if (!user?.username) return;
     fetch("https://app.studiodanv.workers.dev/api/project/action", {
       "headers": {
-        "accept": "*/*",
-        "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
-        "content-type": "application/json",
         "x-client-type": "StudioDANV-Web"
       },
-      "referrer": "https://turbows.pages.dev/",
-      "referrerPolicy": "strict-origin-when-cross-origin",
       "body": JSON.stringify({
         fileId: fid,
         action: "add_view",
@@ -29,9 +24,8 @@
         sessionToken: user.sessionToken
       }),
       "method": "POST",
-      "mode": "cors",
       "credentials": "omit"
     });
   }
 }();
-// javascript:!async function(){const e=(await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(e=>e.text())).split(/\r?\n/).filter(Boolean);for(;;)for(const o of e)n(o),await t(400);function t(e){return new Promise(t=>setTimeout(t,e))}function n(e){const t=localStorage.getItem("danv_user");t?.username&&fetch("https://app.studiodanv.workers.dev/api/project/action",{headers:{accept:"*/*","accept-language":"vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5","content-type":"application/json","x-client-type":"StudioDANV-Web"},referrer:"https://turbows.pages.dev/",referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify({fileId:e,action:"add_view",username:t.username,sessionToken:t.sessionToken}),method:"POST",mode:"cors",credentials:"omit"})}}();
+// javascript:!async function(){const t=(await fetch("https://raw.githubusercontent.com/anuser88/my-tools-sibu/master/targets.txt").then(t=>t.text())).split(/\r?\n/).filter(Boolean);for(;;)for(const o of t)n(o),await e(400);function e(t){return new Promise(e=>setTimeout(e,t))}function n(t){const e=JSON.parse(localStorage.getItem("danv_user"));e?.username&&fetch("https://app.studiodanv.workers.dev/api/project/action",{headers:{"x-client-type":"StudioDANV-Web"},body:JSON.stringify({fileId:t,action:"add_view",username:e.username,sessionToken:e.sessionToken}),method:"POST",credentials:"omit"})}}();
